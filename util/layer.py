cf450c712a434eefe5d83ab7581bb4e8b02ce0ac
astaka-pe <astaka1119@g.ecc.u-tokyo.ac.jp>
2022-11-17 22:40:34 +0900

    :boom: update meshconv

--- util/layer.py
+++ util/layer.py
@@ -3,18 +3,12 @@ import torch.nn as nn
 
 
 class MeshConv(nn.Module):
-    def __init__(self, in_channels, out_channels, mesh, device="cuda:0"):
+    def __init__(self, in_channels, out_channels, mesh, bias=True):
         super(MeshConv, self).__init__()
-        self.F = mesh.meshconvF.to(device)
-        self.w = nn.Linear(in_channels, out_channels)
+        self.register_buffer("F", mesh.meshconvF)
+        self.w = nn.Linear(in_channels, out_channels, bias=bias)
     
     def forward(self, x):
         out = torch.sparse.mm(self.F, x)     # (Nv, Cin)
-        out = self.w(out)               # (Nv, Cout)
+        out = self.w(out)                    # (Nv, Cout)
         return out
-    
-    def _get_filter(self, mesh_lap):
-        lambda_max, _ = torch.lobpcg(mesh_lap, k=1)
-        F = 2.0 * mesh_lap / lambda_max[0] - torch.eye(mesh_lap.shape[0])
-        F = F.to_sparse()
-        return F
